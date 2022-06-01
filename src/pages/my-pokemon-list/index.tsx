b63b8017fc8b00424fb246810701e22ccae70710
nifemi <deyeminifemi@gmail.com>
2022-06-01 03:27:53 +0100

    Setup release popup messages

--- src/pages/my-pokemon-list/index.tsx
+++ src/pages/my-pokemon-list/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {useAppDispatch, useAppSelector} from "../../redux/hooks";
 import {getCaughtPokemon, releasePokemon} from "../../redux/my-pokemon.slice";
 import {CaughtPokemon} from "types/pokemon";
 import PixelatedButton from "components/pixelated-button";
 import styles from "./index.module.css"
+import Popup from "components/popup";
+import Typewriter from "components/typewriter/typewriter";
 
 const MyPokemonList = () => {
     const myPokemon = useAppSelector(getCaughtPokemon);
     const dispatch = useAppDispatch();
 
-    const handleReleasePokemon = (pokemonId: string, pokemonName: string) => () => {
-        dispatch(releasePokemon({pokemonId, pokemonName}));
+    const [deletedPokemon, setDeletedPokemon] = React.useState("");
+
+    const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+
+    const handleReleasePokemon = (pokemon: CaughtPokemon) => () => {
+        clearTimeout(timeoutRef.current);
+
+        dispatch(releasePokemon({pokemonId: pokemon.id, pokemonName: pokemon.name}));
+
+        setDeletedPokemon(pokemon.nickname);
+
+        //Wipe message after 3 seconds but also store the timeout incase we need to reset the timeout if the user releases another pokemon
+        timeoutRef.current = setTimeout(() => setDeletedPokemon(""), 3000);
     }
 
     return (
@@ -26,13 +39,18 @@ const MyPokemonList = () => {
                             {pokemon.name}
                         </h3>
                         <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
-                        <PixelatedButton onClick={handleReleasePokemon(pokemon.id, pokemon.name)}
-                                         className={styles.myPokemonListItemRelease}>
+                        <PixelatedButton
+                            onClick={handleReleasePokemon(pokemon)}
+                            className={styles.myPokemonListItemRelease}
+                        >
                             Release
                         </PixelatedButton>
                     </li>
                 ))}
             </ul>
+            <Popup isVisible={!!deletedPokemon}>
+                <Typewriter text={`${deletedPokemon} has been released!`}/>
+            </Popup>
         </div>
     );
 };
