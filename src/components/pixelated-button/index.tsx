245bb44f83b157c515d2a85441166767939f97d7
nifemi <deyeminifemi@gmail.com>
2022-06-01 01:56:16 +0100

    pixelated button

---
+++ src/components/pixelated-button/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./index.module.css";
+import {classNames} from "../../utils/classNames";
+
+interface Props {
+    children: React.ReactNode;
+    className?: string;
+    onClick: React.EventHandler<React.MouseEvent>;
+}
+
+
+//Pixelated button inspired by https://codepen.io/YoannM/pen/yyExEO
+const PixelatedButton: React.FC<Props> = ({children, className = '', ...props}) => {
+    return (
+        <button className={classNames(styles.pixelatedButton, className)} {...props}>
+            <span className={styles.pixelatedButtonText}>
+                {children}
+            </span>
+        </button>
+    );
+};
+
+export default PixelatedButton;
