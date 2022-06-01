8d560f711b16879eb7904413b65fda9e03b89c5c
nifemi <deyeminifemi@gmail.com>
2022-06-01 19:59:07 +0100

    Added new helper function and generated separate stores for tests

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,21 +1,37 @@
-import {render as rtlRender} from "@testing-library/react";
-import {store} from "../redux/store";
+import {render as rtlRender, screen, waitFor} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-
-const Wrapper = ({children}) => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        {children}
-      </BrowserRouter>
-    </Provider>
-  )
-}
+import {configureStore} from "@reduxjs/toolkit";
+import {pokemonApi} from "api/pokemon";
+import myPokemonSlice from "../redux/my-pokemon.slice";
 
 const render = (ui, options) => {
+  const store = configureStore({
+    reducer: {
+      [pokemonApi.reducerPath]: pokemonApi.reducer,
+      myPokemon: myPokemonSlice.reducer
+    },
+    middleware: getDefaultMiddleware => {
+      return getDefaultMiddleware().concat(pokemonApi.middleware)
+    }
+  });
+
+  const Wrapper = ({children}) => {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+      </Provider>
+    )
+  }
+
   return rtlRender(ui, {wrapper: Wrapper, ...options})
 }
 
+const awaitLoading = async () => {
+  await waitFor(() => expect(screen.queryByText("Extracting local database")).toBeNull());
+}
+
 export * from "@testing-library/react"
-export {render}
+export {render, awaitLoading}
